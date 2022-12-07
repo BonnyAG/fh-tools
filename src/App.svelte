52661493c21cd91c7bbfd7ee4b6871939fa82542
Adam Bonny <agbonnyh@gmail.com>
2022-12-06 20:27:51 -0700

    Added footer

--- src/App.svelte
+++ src/App.svelte
@@ -26,7 +26,7 @@
           From: "opacity-100"
           To: "opacity-0"
       -->
-      <div class="fixed inset-0 bg-gray-600 bg-opacity-75"></div>
+      <div class="fixed inset-0 bg-gray-600 bg-opacity-75 {sidebarOpen ? "" : "transition hidden"}"></div>
   
       <div class="fixed inset-0 z-40 flex transition {sidebarOpen ? " ease-in-out duration-300 transform translate-x-0" : "-translate-x-full ease-in-out duration-300 transform"}">
         <!--
@@ -63,7 +63,7 @@
           <div class="h-0 flex-1 overflow-y-auto pt-5 pb-4">
             <div class="flex flex-shrink-0 items-center px-4">
               <span class="p-4 bg-blue-100 rounded-md">🌳</span>
-              <h1 class="font-bold text-2xl ml-2">FH Tools</h1>
+              <h1 class="font-bold text-2xl ml-2">F.H. Tools</h1>
             </div>
             <nav class="mt-5 space-y-1 px-2">
               <!-- Current: "bg-gray-100 text-gray-900", Default: "text-gray-600 hover:bg-gray-50 hover:text-gray-900" -->
@@ -131,7 +131,7 @@
       </div>
     </div>
     <div class="flex flex-1 flex-col md:pl-64">
-      <div class="sticky top-0 z-10 bg-white pl-1 pt-1 sm:pl-3 sm:pt-3 md:hidden">
+      <div class="sticky top-0 z-20 bg-white pl-1 pt-1 sm:pl-3 sm:pt-3 md:hidden">
         <button type="button" on:click={updateSidebar}  class="-ml-0.5 -mt-0.5 inline-flex h-12 w-12 items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-500">
           <span class="sr-only">Open sidebar</span>
           <!-- Heroicon name: outline/bars-3 -->
@@ -140,12 +140,15 @@
           </svg>
         </button>
       </div>
-      <main class="flex-1">
-        <div class="py-6 z-10">
+      <div class="flex flex-col sm:h-screen">
+        <main class="flex-1 py-6 z-10">
           <!-- Have components after this -->
           <CitationContainer />
-        </div>
-      </main>
+        </main>
+        <footer class="bg-gradient-to-t from-white via-white to-transparent z-30 sticky bottom-0 text-center sm:mb-0 sm:text-right h-10 sm:mr-8">
+          <p class="text-slate-400">&copy; {new Date().getFullYear()} Adam Bonny. All rights reserved. Built with <a class="text-blue-600 hover:text-blue-400 hover:underline" href="https://github.com/BonnyAG/fh-tools">Github</a> & <a class="text-blue-600 hover:text-blue-400 hover:underline" href="https://vercel.com">Vercel</a></p>
+        </footer>
+      </div>
     </div>
   </div>
 </main>
