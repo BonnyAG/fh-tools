5822b96a9c03950674fb6ea727b12d4b707d532a
Adam Bonny <agbonnyh@gmail.com>
2025-05-31 21:13:51 +0200

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,83 +1,85 @@
+<script lang="js">
+    // Import Carbon Components
+    import { Dropdown } from "carbon-components-svelte";
+
+    // Import Layout
+    import Sidebar from "$lib/utilities/Sidebar.svelte";
+
+    // Import Citations
+    import CitationContainer from "$lib/utilities/CitationContainer.svelte";
+    import Census from "$lib/basic_citations/Census.svelte";
+    import Church from "$lib/basic_citations/Church.svelte";
+    import FamilyTreeHousehold from "$lib/basic_citations/FamilyTreeHousehold.svelte";
+    import FamilyTreePersonal from "$lib/basic_citations/FamilyTreePersonal.svelte";
+    import Newspaper from "$lib/basic_citations/Newspaper.svelte";
+    import FindAGrave from "$lib/basic_citations/FindAGrave.svelte";
+    import PersonalKnowledge from "$lib/basic_citations/PersonalKnowledge.svelte";
+
+    /** Tracks which citation template is being displayed
+     * @type {string}
+     */
+    let citationType = $state("tree-personal");
+
+    /** List of all citation options
+     * @const
+     * @type {{id: string, text: string}[]}
+     */
+    const CITATION_OPTIONS = [
+        { id: "tree-personal", text: "Family Tree Personal" },
+        { id: "tree-household", text: "Family Tree Household" },
+        { id: "census", text: "Census Record" },
+        { id: "church", text: "Church & Civil Record" },
+        { id: "newspaper", text: "Newspaper Article" },
+        { id: "findagrave", text: "Find A Grave Memorial" },
+        { id: "knowledge-personal", text: "Personal Knowledge" },
+    ];
+</script>
+
 <svelte:head>
     <title>Basic Citation Builder - F.H Tools</title>
 </svelte:head>
 
-<script lang="js">
-	// Import Carbon Components
-	import { Dropdown } from "carbon-components-svelte";
-
-	// Import Layout
-	import Sidebar from '$lib/utilities/Sidebar.svelte';
-
-	// Import Citations
-	import CitationContainer from '$lib/utilities/CitationContainer.svelte';
-    import Census from '$lib/basic_citations/Census.svelte';
-    import Church from '$lib/basic_citations/Church.svelte';
-    import FamilyTreeHousehold from '$lib/basic_citations/FamilyTreeHousehold.svelte';
-    import FamilyTreePersonal from '$lib/basic_citations/FamilyTreePersonal.svelte'
-	import Newspaper from "$lib/basic_citations/Newspaper.svelte";
-	import FindAGrave from '$lib/basic_citations/FindAGrave.svelte';
-    import PersonalKnowledge from '$lib/basic_citations/PersonalKnowledge.svelte';
-
-	/** Tracks which citation template is being displayed
-	 * @type {string} 
-	*/
-	let citationType = $state("tree-personal");
-
-	/** List of all citation options
-	 * @const
-	 * @type {{id: string, text: string}[]}
-	*/
-	const CITATION_OPTIONS = [
-		{ id: "tree-personal", text: "Family Tree Personal"},
-		{ id: "tree-household", text: "Family Tree Household"},
-		{ id: "census", text: "Census Record"},
-		{ id: "church", text: "Church & Civil Record"},
-		{ id: "newspaper", text: "Newspaper Article"},
-		{ id: "findagrave", text: "Find A Grave Memorial"},
-		{ id: "knowledge-personal", text: "Personal Knowledge"},
-	]
-</script>
-
 <Sidebar selected="home">
-	{#snippet container()}
-		<div  class="h-auto">
-			<!-- HEADER -->
-			<section class="mx-auto md:max-w-7xl">
-				<h1 class="text-2xl font-semibold text-gray-900">Basic Citation Builder</h1>
-			</section>
-			<!-- MAIN CONTENT -->
-			<section class="mx-auto md:max-w-7xl">
-				<div class="py-1">
-					<!-- SELECT CITATION TYPE -->
-					<Dropdown
-						titleText="Citation Type"
-						placeholder="Select citation type"
-						items={CITATION_OPTIONS}
-						size="xl"
-						bind:selectedId={citationType}
-					/>
-					
-					<CitationContainer>
-						<!-- DISPLAY CITATION -->
-						{#if citationType === "tree-personal"}
-							<FamilyTreePersonal />
-						{:else if citationType === "tree-household"}
-							<FamilyTreeHousehold />
-						{:else if citationType === "census"}
-							<Census />
-						{:else if citationType === "church"}
-							<Church />
-						{:else if citationType === "newspaper"}
-							<Newspaper />
-						{:else if citationType === "findagrave"}
-							<FindAGrave />
-						{:else if citationType === "knowledge-personal"}
-							<PersonalKnowledge />
-						{/if}
-					</CitationContainer>
-				</div>
-			</section>
-		</div>
-	{/snippet}
+    {#snippet container()}
+        <div class="h-auto">
+            <!-- HEADER -->
+            <section class="mx-auto md:max-w-7xl">
+                <h1 class="text-2xl font-semibold text-gray-900">
+                    Basic Citation Builder
+                </h1>
+            </section>
+            <!-- MAIN CONTENT -->
+            <section class="mx-auto md:max-w-7xl">
+                <div class="py-1">
+                    <!-- SELECT CITATION TYPE -->
+                    <Dropdown
+                        titleText="Citation Type"
+                        placeholder="Select citation type"
+                        items={CITATION_OPTIONS}
+                        size="xl"
+                        bind:selectedId={citationType}
+                    />
+
+                    <CitationContainer>
+                        <!-- DISPLAY CITATION -->
+                        {#if citationType === "tree-personal"}
+                            <FamilyTreePersonal />
+                        {:else if citationType === "tree-household"}
+                            <FamilyTreeHousehold />
+                        {:else if citationType === "census"}
+                            <Census />
+                        {:else if citationType === "church"}
+                            <Church />
+                        {:else if citationType === "newspaper"}
+                            <Newspaper />
+                        {:else if citationType === "findagrave"}
+                            <FindAGrave />
+                        {:else if citationType === "knowledge-personal"}
+                            <PersonalKnowledge />
+                        {/if}
+                    </CitationContainer>
+                </div>
+            </section>
+        </div>
+    {/snippet}
 </Sidebar>
